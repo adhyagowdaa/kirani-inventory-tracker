a994f92431bdb3197d6aa33bf31d6f1c9a1ccac3
Adhya Jaydev <adhyajaydev@Adhyas-MacBook-Air.local>
2026-06-05 11:50:09 +0530

    Integrated Login and Dashboard components

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../controllers/authController');
+const { register, login} = require('../controllers/authController');
 
 // This means when the frontend calls /api/auth/register, it runs the register function
 router.post('/register', register);
